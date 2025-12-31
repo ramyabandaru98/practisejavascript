7b8602abb9317cb184df3534c6dbf506d9fede30
ramyabandaru98 <89702365+ramyabandaru98@users.noreply.github.com>
2025-12-31 18:15:34 +0530

    practise programs

---
+++ variablestest.js
@@ -0,0 +1,58 @@
+//addition//
+let a=2;
+let b=3;
+const c=(a+b);
+console.log("Addition:"+c);
+
+//subtraction//
+let d=10;
+let e=11;
+const f=(d-e);
+console.log("Subtraction:"+f);
+
+//division//
+let x=20;
+let y=5;
+const g=(x/y);
+console.log("division:"+g)
+
+//multiplication//
+let t=4;
+let u=4;
+const h=(t*u);
+console.log("multiplication:"+h);
+
+//circle//
+//area of circle= πr2
+//r is the radius of the circle (distance from the center to the edge)
+//π (pi) is approximately 3.1416
+const r= 10;
+const PI= 3.14
+const area= (("Area of circle=") + PI * r*r);
+console.log(area);
+
+//rectangle//
+//area of a rectangle= Area= length*width
+//length=longer side
+//width= shorter side
+let length= 100;
+let width =50;
+const o =(length* width);
+const Area =(("Area of a rectangle=") + o);
+console.log(Area);
+
+//cylinder//
+//Lateral surface of a cylinder = curved side area only.
+//area of cylinder(curved area only)= 2 * π * r * h
+//total area of cylinder= 2πr(h+r)
+//r= radius
+//h= height
+// value of π= 31.4
+
+const radius= 4;
+const height= 2;
+const pi= 31.4;
+const value=(("Lateral area of cylinder=")+ 2* pi* radius*height );
+const total=(("Total area of a cylinder=")+2*pi*radius*(radius+height));
+console.log(value);
+console.log(total);
