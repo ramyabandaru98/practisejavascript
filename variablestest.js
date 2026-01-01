e5cd549e1085690ed85982986c7c82671ce336cf
ramyabandaru98 <89702365+ramyabandaru98@users.noreply.github.com>
2026-01-01 22:07:04 +0530

    percentage program

--- variablestest.js
+++ variablestest.js
@@ -56,3 +56,32 @@ const value=(("Lateral area of cylinder=")+ 2* pi* radius*height );
 const total=(("Total area of a cylinder=")+2*pi*radius*(radius+height));
 console.log(value);
 console.log(total);
+
+
+//NAN
+let a5=10;
+let a6="sai";
+console.log(a5/a6);
+
+
+//Ramesh and Suresh are two friends. Ramesh salary is 10,000 and Suresh Salary is 15,000.
+//  I wanted ramesh and suresh salary increase by 25% hike.
+//  Write a code where i see the total salary of Ramesh and suresh after 25% Hike.
+//formula perc/100 *originalvalue
+
+// Ramesh’s original salary = 10,000
+//25% of 10,000 = 2,500
+//total salary after the hike =10,000 + 2,500 = 12,500
+
+//suresh original salary= 15,000
+//25% of 15,000 = 3,750
+//Total salary after the hike =15,000 + 3,750 = 18,750
+
+let rameshsalary= 10000;
+let sureshsalary= 15000;
+let hikepercentage= 25
+const Newsalaryoframesh = ((hikepercentage/100) * rameshsalary);
+const Newsalaryofsuresh =((hikepercentage/100) * sureshsalary);
+console.log("salary of ramesh after hike=",rameshsalary+ Newsalaryoframesh);
+console.log("salary of suresh after hike=",sureshsalary+ Newsalaryofsuresh);
+
